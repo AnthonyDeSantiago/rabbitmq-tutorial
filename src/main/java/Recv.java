dba9f423795b10ba78a5fbf67d22834dceaee449
Anthony De Santiago <anthony.de_santiago@us.af.mil>
2024-06-26 09:25:47 -0400

    Worker_1

--- src/main/java/Recv.java
+++ src/main/java/Recv.java
@@ -6,7 +6,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Recv {
 
-    private final static String QUEUE_NAME = "hello";
+    private final static String TASK_QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -14,13 +14,27 @@ public class Recv {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
 
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            String message = new String(delivery.getBody(), "UTF-8");
+
             System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } catch(Exception e) {
+                System.out.println("It broke: " + e.getMessage());
+            } finally {
+                System.out.println(" [x] Done");
+            }
         };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+    }
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') Thread.sleep(1000);
+        }
     }
 }
