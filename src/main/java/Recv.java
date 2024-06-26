a925701b0a1c6bf7ef4cffe47123422dffa6902e
Anthony De Santiago <anthony.de_santiago@us.af.mil>
2024-06-26 09:51:11 -0400

    Worker tutorial complete

--- src/main/java/Recv.java
+++ src/main/java/Recv.java
@@ -2,22 +2,21 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import java.nio.charset.StandardCharsets;
 
 public class Recv {
 
-    private final static String TASK_QUEUE_NAME = "hello";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        channel.basicQos(1); // accept only one unack-ed message at a time (see below)
+        channel.basicQos(1);
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
@@ -25,19 +24,23 @@ public class Recv {
             System.out.println(" [x] Received '" + message + "'");
             try {
                 doWork(message);
-            } catch (Exception e) {
-                System.out.println("It broke: " + e.getMessage());
             } finally {
                 System.out.println(" [x] Done");
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-        boolean autoAck = false;
-        channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> { });
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
-    private static void doWork(String task) throws InterruptedException {
-        for (char ch: task.toCharArray()) {
-            if (ch == '.') Thread.sleep(1000);
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 }
--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -1,23 +1,26 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-
-import java.nio.charset.StandardCharsets;
+import com.rabbitmq.client.MessageProperties;
 
 public class Send {
 
-    private final static String QUEUE_NAME = "hello";
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
             String message = String.join(" ", argv);
 
-            channel.basicPublish("", "hello", null, message.getBytes());
+            channel.basicPublish("", TASK_QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
+
 }
