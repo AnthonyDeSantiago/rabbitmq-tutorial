45beb1094cf553cac2be6b352f90c63c8a055390
PROJECTSYNERYGA\charles.foshee <cfoshee.03@gmail.com>
2024-06-26 11:16:13 -0400

    routing

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -1,24 +1,31 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
 
 public class Send {
 
-    private static final String EXCHANGE_NAME = "logs";
+    private static final String EXCHANGE_NAME = "direct_logs";
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
 
-            String message = argv.length < 1 ? "info: Hello World!" :
-                    String.join(" ", argv);
+            String severity = getSeverity(argv);
+            String message = getMessage(argv);
 
-            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + message + "'");
+            channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + severity + "':'" + message + "'");
         }
     }
+
+    public static String getSeverity(String[] argv) {
+        return argv[0];
+    }
+    public static String getMessage(String[] argv) {
+        return argv[1];
+    }
+    //..
 }
